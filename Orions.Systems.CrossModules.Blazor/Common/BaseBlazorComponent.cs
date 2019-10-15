9a05ca376396a97e5cd784bf5154eefe983847ca
deyan <deyan@orionswave.com>
2019-10-15 14:46:30 +0300

    Common blazor vm components

---
+++ Orions.Systems.CrossModules.Blazor/Common/BaseBlazorComponent.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orions.Systems.CrossModules.Blazor
+{
+	public class BaseBlazorComponent : BaseOrionsComponent
+	{
+	}
+
+	public class BaseBlazorComponent<VmType> : BaseBlazorComponent
+		where VmType : BaseVm, new()
+	{
+		VmType _dataContext = null;
+
+		public VmType DataContext
+		{
+			get
+			{
+				return _dataContext;
+			}
+
+			set
+			{
+				_dataContext = value;
+				if (value != null)
+				{
+					value.PropertyChanged += DataContext_PropertyChanged;
+				}
+			}
+		}
+
+		bool _initialized = false;
+
+		public BaseBlazorComponent()
+		{
+			// Create a default Vm instance.
+			DataContext = new VmType();
+		}
+
+		void DataContext_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if (_initialized == false)
+				return;
+
+			var t = this.InvokeAsync(() =>
+			{
+				this.StateHasChanged();
+			});
+		}
+
+		protected override Task OnInitializedAsync()
+		{
+			_initialized = true;
+			return base.OnInitializedAsync();
+		}
+	}
+}
---
+++ Orions.Systems.CrossModules.Blazor/Common/BlazorCommand.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using Orions.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Orions.Systems.CrossModules.BlazorSample
+{
+	/// <summary>
+	/// Provides conversion mechanism between the Blazor event system and the DefaultCommand pattern.
+	/// </summary>
+	public class BlazorCommand : DefaultCommand, IHandleEvent
+	{
+		public BlazorCommand()
+		{
+		}
+
+		public static implicit operator EventCallback(BlazorCommand comm)
+		{
+			return new EventCallback(comm, null);
+		}
+
+		public Task HandleEventAsync(EventCallbackWorkItem item, object arg)
+		{
+			this.Execute(null);
+			return Task.CompletedTask;
+		}
+	}
+}
