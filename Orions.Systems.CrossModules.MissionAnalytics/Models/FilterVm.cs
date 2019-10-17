dffbf2a3bfed7bc5edb20d728b923e0639bf2e2e
Diana <diana@orionswave.com>
2019-10-17 13:13:19 +0300

    Switching Mission Analytics to the shared Vm design (wip)

---
+++ Orions.Systems.CrossModules.MissionAnalytics/Models/FilterVm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Orions.Systems.CrossModules.Blazor;
+
+namespace Orions.Systems.CrossModules.MissionAnalytics
+{
+	public class FilterVm : BlazorVm
+	{
+		public List<SelectListItem> MissionInstanceOptions { get; set; }
+		
+		public string SelectedMissionInstance
+		{
+			get => GetSelectedValue(MissionInstanceOptions);
+			set => SetSelectedValue(MissionInstanceOptions, value);
+		}
+
+		public List<SelectListItem> TimeRangeOptions{ get; set; }
+
+		public string SelectedTimeRange
+		{
+			get => GetSelectedValue(TimeRangeOptions);
+			set => SetSelectedValue(TimeRangeOptions, value);
+		}
+
+		public FilterVm()
+		{
+			MissionInstanceOptions= new List<SelectListItem>();
+			TimeRangeOptions = new List<SelectListItem>();
+		}
+
+		private static string GetSelectedValue(
+			IEnumerable<SelectListItem> items)
+		{
+			return items?.FirstOrDefault(it => it.Selected)?.Value ?? "";
+		}
+
+		private static void SetSelectedValue(
+			IEnumerable<SelectListItem> items, 
+			string value)
+		{
+			if (items == null) throw new ArgumentException(nameof(items));
+
+			foreach (var option in items)
+			{
+				option.Selected = false;
+			}
+
+			var selected = items.FirstOrDefault(it => it.Value == value);
+			if (selected == null) return;
+			selected.Selected = true;
+		}
+	}
+}
