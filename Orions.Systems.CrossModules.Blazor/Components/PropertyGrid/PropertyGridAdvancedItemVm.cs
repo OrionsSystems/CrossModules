23489ccfd8493443bb332ccfd8a3e0efd7d2de8a
Venelin Kehayov <venelin@orionswave.com>
2019-10-30 17:55:04 +0200

    Add property grid component

---
+++ Orions.Systems.CrossModules.Blazor/Components/PropertyGrid/PropertyGridAdvancedItemVm.cs
@@ -0,0 +1,26 @@
+﻿using Orions.Common;
+using Orions.SDK;
+
+namespace Orions.Systems.CrossModules.Blazor.Components.PropertyGrid
+{
+	public class PropertyGridAdvancedItemVm : BaseVm
+	{
+		public PropertyGridUtility Utility { get; set; }
+
+		public ViewModelProperty<PropertyGridItemUtilityData> DataProp { get; private set; } = new ViewModelProperty<PropertyGridItemUtilityData>();
+
+		public DefaultCommand CustomExpandCommand { get; private set; } = new DefaultCommand();
+
+		public PropertyGridAdvancedItemVm()
+		{
+			//CustomExpandCommand.Owner = this;
+		}
+
+		public string GroupName { get; set; }
+
+		public override string ToString()
+		{
+			return DataProp?.Value.ToString();
+		}
+	}
+}
