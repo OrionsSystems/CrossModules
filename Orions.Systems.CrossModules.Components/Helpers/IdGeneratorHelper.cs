75350d92fab90c98af3aaef9a06ed9ca04e11a25
Venelin Kehayov <venelin@orionswave.com>
2019-11-14 17:45:43 +0200

    Add checkbox component

---
+++ Orions.Systems.CrossModules.Components/Helpers/IdGeneratorHelper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public static class IdGeneratorHelper
+	{
+		public static string Generate(string prefix)
+		{
+			return prefix + Guid.NewGuid();
+		}
+	}
+}
