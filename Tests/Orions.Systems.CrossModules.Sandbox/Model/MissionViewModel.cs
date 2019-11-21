e1e9a25694a7beb45cb4e3d4c5342a204a7e1b3e
Venelin Kehayov <venelin@orionswave.com>
2019-11-21 13:09:55 +0100

    Add mission and workflow models

---
+++ Tests/Orions.Systems.CrossModules.Sandbox/Model/MissionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Orions.Infrastructure.HyperMedia;
+using Orions.Node.Common;
+using Orions.SDK.Utilities;
+
+namespace Orions.Systems.CrossModules.Sandbox.Model
+{
+	public class MissionViewModel
+	{
+		public HyperDocument Document { get; set; }
+
+		public HyperMission Mission { get; set; }
+
+		public string MissionId { get; set; }
+
+		public string Name { get; set; }
+
+		public bool IsActive { get; set; }
+
+		public string MissionInstanceId { get; set; }
+
+		public HyperMissionInstanceStatus InstanceStatus { get; set; }
+
+		public string InstanceDescription { get; set; }
+
+		public string Status { get; set; }
+	}
+}
---
+++ Tests/Orions.Systems.CrossModules.Sandbox/Model/WorkflowViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Orions.Infrastructure.HyperMedia;
+using Orions.Node.Common;
+
+namespace Orions.Systems.CrossModules.Sandbox.Model
+{
+	public class WorkflowViewModel
+	{
+		public string Name { get; set; }
+		public string WorkflowId { get; set; }
+		public HyperDocument Document { get; set; }
+		public HyperWorkflow Source { get; set; }
+		public HyperWorkflowStatus[] WorkflowStatuses { get; set; }
+
+		public bool HasInstances
+		{
+			get
+			{
+				if (WorkflowStatuses != null && this.WorkflowStatuses.Length > 0)
+				{
+					return true;
+				}
+
+				return false;
+			}
+		}
+	}
+}
