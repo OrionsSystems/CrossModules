3114d503939289aa03fab6ce2763fef6f6ac84aa
Venelin Kehayov <venelin@orionswave.com>
2019-12-05 19:16:22 +0100

    Moved dashboad layout into component

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/DashboardData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orions.Systems.CrossModules.Components
+{
+    public class DashboardData
+    {
+        public List<DashboardRow> Rows { get; set; }
+
+        public DashboardData()
+        {
+            Rows = new List<DashboardRow>();
+        }
+    }
+
+    public class DashboardRow
+    {
+        public string Id { get; set; } = IdGeneratorHelper.Generate("db-row-");
+        public LinkedList<DashboardColumn> Columns { get; set; }
+
+        public DashboardRow()
+        {
+            Columns = new LinkedList<DashboardColumn>();
+        }
+    }
+
+    public class DashboardColumn
+    {
+        public string Id { get; set; } = IdGeneratorHelper.Generate("db-col-");
+        public int Size { get; set; }
+        public int Order { get; set; }
+    }
+}
