67aa89e3439e36dc8e4b5b9cc078e60ed53a6a9b
Venelin Kehayov <venelin@orionswave.com>
2020-01-07 15:58:37 +0100

    Add stacked series dashboard widget

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/ReportTelericStackedSeriesChartWidget.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Telerik.Blazor;
+
+namespace Orions.Systems.CrossModules.Components
+{
+    public class ReportTelericStackedSeriesChartWidget : ReportTelericBaseChartWidget, IDashboardWidget
+    {
+        public override string Label { get; set; } = "Report Stacked Series Chart (Teleric)";
+
+        public override ChartSeriesType ChartSeriesType { get; set; } = ChartSeriesType.Column;
+
+        public override bool IsChartSeriesStackEnabled { get; set; } = true;
+
+        public override Type GetViewComponent()
+        {
+            return typeof(ReportTelericStackedSeriesChart);
+        }
+    }
+}
