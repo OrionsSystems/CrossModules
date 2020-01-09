94bc21b18f370ac41331f7bc86124a3272ce95b9
Venelin Kehayov <venelin@orionswave.com>
2020-01-09 12:39:14 +0100

    Add base syncfusion widget

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Syncfusion/ReportSyncfusionSeriesChartWidget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Orions.Systems.CrossModules.Components
+{
+    public class ReportSyncfusionSeriesChartWidget : ReportSyncfusionBaseChartWidget, IDashboardWidget
+    {
+        public override string Label { get; set; } = "Report Stacked Series Chart (Syncfusion)";
+
+        public override Type GetViewComponent()
+        {
+            return typeof(ReportSyncfusionSeriesChart);
+        }
+    }
+}
