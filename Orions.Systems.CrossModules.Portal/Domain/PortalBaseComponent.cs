9996d1b29e939fa29007dc20d33665cdeefbe1d4
Nikolay Stoychev <n_stoychev@yahoo.com>
2020-01-10 18:33:29 +0100

    Moved everything in domain folder

--- Orions.Systems.CrossModules.Portal/Domain/PortalBaseComponent.cs
+++ Orions.Systems.CrossModules.Portal/Domain/PortalBaseComponent.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using Orions.Systems.CrossModules.Components;
+using Microsoft.AspNetCore.Authorization;
 using Orions.Infrastructure.HyperMedia;
 using Orions.Node.Common;
-using Microsoft.AspNetCore.Authorization;
-using Orions.Infrastructure.HyperMedia.Formats.Mpeg;
-using Microsoft.AspNetCore.Components.Authorization;
+using Orions.Systems.CrossModules.Components;
 using Orions.Systems.CrossModules.Portal.Providers;
 
 namespace Orions.Systems.CrossModules.Portal.Domain
@@ -34,7 +30,7 @@ namespace Orions.Systems.CrossModules.Portal.Domain
 
 		public async Task AuthenticateAsync(CustomAuthenticationStateProvider provider)
 		{
-			await InitHyperStoreInstanceAsync();				
+			await InitHyperStoreInstanceAsync();
 
 			var authState = await provider.GetAuthenticationStateAsync();
 
