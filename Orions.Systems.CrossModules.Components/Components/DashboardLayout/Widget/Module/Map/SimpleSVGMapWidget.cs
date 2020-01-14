085ffd5a304376ad801ab377a68502d79928501c
Venelin Kehayov <venelin@orionswave.com>
2020-01-14 19:16:23 +0100

    Init svg widget

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Map/SimpleSVGMapWidget.cs
@@ -0,0 +1,10 @@
+﻿namespace Orions.Systems.CrossModules.Components
+{
+   public class SimpleSVGMapWidget : DashboardWidget, IDashboardWidget
+   {
+      public SimpleSVGMapWidget()
+      {
+         this.Label = "Simple SVG Map Widget";
+      }
+   }
+}
