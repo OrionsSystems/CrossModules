49b59106c3f5da40f0b5dd17a16b653f1a9774b4
Venelin Kehayov <venelin@orionswave.com>
2020-01-15 18:00:02 +0100

    Init metadata review widget

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/MetadataReview/MetadataTagReviewVm.cs
@@ -0,0 +1,12 @@
+﻿using Orions.Common;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	[Config(typeof(MetadataTagReviewWidget))]
+	public class MetadataTagReviewVm : WidgetVm<MetadataTagReviewWidget>
+	{
+		public MetadataTagReviewVm()
+		{
+		}
+	}
+}
