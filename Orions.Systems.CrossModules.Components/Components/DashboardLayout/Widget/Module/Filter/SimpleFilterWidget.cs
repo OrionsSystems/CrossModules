5906bed7a327f10eaa7fa315aa039958df46c0e1
deyan <deyan@orionswave.com>
2020-01-15 22:35:57 +0200

    Simple Filter updates for multi filter group system, added date time filtering

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Filter/SimpleFilterWidget.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Filter/SimpleFilterWidget.cs
@@ -1,14 +1,23 @@
-﻿using System;
+﻿using Orions.Common;
+using System;
 
 namespace Orions.Systems.CrossModules.Components
 {
    public class SimpleFilterWidget : ReportBaseWidget
    {
+      /// <summary>
+      /// Persist the previous selection of the user.
+      /// </summary>
+      [UniBrowsable(false)]
       public string[] Filters { get; set; }
+
+      public bool ShowTextLabelSelection { get; set; } = true;
+
+      public bool ShowDateTimeSelection { get; set; } = true;
 
       public SimpleFilterWidget()
       {
-         this.Label = "Simple Filter";
+         this.Label = "Simple Filter"; // Title of this widget.
       }
    }
 }
