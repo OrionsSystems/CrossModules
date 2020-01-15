7543646b30bf074718be2a6e760ae68d774880a7
deyan <deyan@orionswave.com>
2020-01-15 22:37:55 +0200

    Metadata Review tweaks

--- Orions.Systems.CrossModules.Components/Components/MetadataReview/MetadataReview.razor.cs
+++ Orions.Systems.CrossModules.Components/Components/MetadataReview/MetadataReview.razor.cs
@@ -9,41 +9,41 @@ using System.Threading.Tasks;
 
 namespace Orions.Systems.CrossModules.Components
 {
-    public class MetadataReviewBase : BaseBlazorComponent<MetadataReviewVm>
-    {
-        protected NetStore _store;
+   public class MetadataReviewBase : BaseBlazorComponent<MetadataReviewVm>
+   {
+      protected NetStore _store;
 
-        protected override async Task OnInitializedAsync()
-        {
-            _store = await NetStore.ConnectAsyncThrows("http://vladimir:654321@usbellods01wan.orionscloud.com:4580/Execute");
+      protected override async Task OnInitializedAsync()
+      {
+         _store = await NetStore.ConnectAsyncThrows("http://vladimir:654321@usbellods01wan.orionscloud.com:4580/Execute");
 
-            await this.DataContext.Initialize(_store);
+         await this.Vm.Initialize(_store);
 
-            DataContext.PageSize.PropertyChanged += PageSize_PropertyChanged;
-            DataContext.PageNumber.PropertyChanged += PageNumber_PropertyChanged;
+         // No need to do this, there is an embedded mechanism in BlazorComponent that scans all ViewModelProperties<> and updates the state.
+         Vm.PageSize.PropertyChanged += PageSize_PropertyChanged;
+         Vm.PageNumber.PropertyChanged += PageNumber_PropertyChanged;
 
-            await base.OnInitializedAsync();
-        }
+         await base.OnInitializedAsync();
+      }
 
-        private void PageNumber_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            this.InvokeAsync(async () =>
-            {
-                await DataContext.ChangePage(DataContext.PageNumber);
+      private void PageNumber_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+         this.InvokeAsync(async () =>
+         {
+            await Vm.ChangePage(Vm.PageNumber); // This is already done in the razor
+            this.StateHasChanged();
+         });
+      }
 
-                this.StateHasChanged();
-            });
-        }
+      private void PageSize_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+         this.InvokeAsync(async () =>
+         {
+            await Vm.LoadTotalPages();
+            await Vm.LoadHyperTags();
 
-        private void PageSize_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            this.InvokeAsync(async () =>
-            {
-                await DataContext.LoadTotalPages();
-                await DataContext.LoadHyperTags();
-
-                this.StateHasChanged();
-            });
-        }
-    }
+            this.StateHasChanged();
+         });
+      }
+   }
 }
--- Orions.Systems.CrossModules.Components/Components/MetadataReview/MetadataReviewVm.cs
+++ Orions.Systems.CrossModules.Components/Components/MetadataReview/MetadataReviewVm.cs
@@ -9,67 +9,78 @@ using System.Threading.Tasks;
 
 namespace Orions.Systems.CrossModules.Components
 {
-    public class MetadataReviewVm : BlazorVm
-    {
-        private NetStore _store;
-        private HyperDocumentId _metadataSetId;
+   public class MetadataReviewVm : BlazorVm
+   {
+      private NetStore _store;
+      private HyperDocumentId _metadataSetId;
 
-        public ViewModelProperty<List<HyperTag>> HyperTags = new ViewModelProperty<List<HyperTag>>(new List<HyperTag>());
-        public ViewModelProperty<int> PageNumber { get; set; } = new ViewModelProperty<int>(1);
-        public ViewModelProperty<int> PageSize { get; set; } = new ViewModelProperty<int>(8);
-        public ViewModelProperty<long> TotalPages { get; set; } = new ViewModelProperty<long>();
+      public ViewModelProperty<List<HyperTag>> HyperTags = new ViewModelProperty<List<HyperTag>>(new List<HyperTag>());
+      public ViewModelProperty<int> PageNumber { get; set; } = new ViewModelProperty<int>(1);
+      public ViewModelProperty<int> PageSize { get; set; } = new ViewModelProperty<int>(8);
+      public ViewModelProperty<long> TotalPages { get; set; } = new ViewModelProperty<long>();
 
-        public bool PlayerOpened { get; set; } = false;
-        public string PlayerUri { get; set; }
-        public string PlayerId { get; set; }
+      public bool PlayerOpened { get; set; } = false;
+      public string PlayerUri { get; set; }
+      public string PlayerId { get; set; }
 
-        public async Task Initialize(NetStore store)
-        {
-            this._store = store;
+      public int CurrentLoadedPage { get; private set; }
 
-            //_metadataSetId = new HyperDocumentId("d1e73018-e487-4d90-b8e0-8bcd530ed3d9", typeof(HyperMetadataSet));
-            _metadataSetId = new HyperDocumentId("101fa54d-361c-4631-8540-dde128e08205", typeof(HyperMetadataSet));
+      public MetadataReviewVm()
+      {
+      }
 
-            await LoadTotalPages();
-            await LoadHyperTags();
-        }        
+      public async Task Initialize(NetStore store)
+      {
+         this._store = store;
 
-        public async Task LoadTotalPages()
-        {
-            var countArgs = new CountHyperDocumentsArgs(typeof(HyperTag));
+         //_metadataSetId = new HyperDocumentId("d1e73018-e487-4d90-b8e0-8bcd530ed3d9", typeof(HyperMetadataSet));
+         _metadataSetId = new HyperDocumentId("101fa54d-361c-4631-8540-dde128e08205", typeof(HyperMetadataSet));
 
-            var totalTags = await CountHyperDocumentsArgs.CountAsync<HyperTag>(this._store, countArgs);
+         await LoadTotalPages();
+         await LoadHyperTags();
+      }
 
-            TotalPages.Value = totalTags % PageSize == 0 ? totalTags / PageSize : totalTags / (PageSize + 1);
-        }
+      public async Task LoadTotalPages()
+      {
+         var countArgs = new CountHyperDocumentsArgs(typeof(HyperTag));
 
-        public async Task LoadHyperTags()
-        {
-            var findArgs = new FindHyperDocumentsArgs(typeof(HyperTag));
+         var totalTags = await CountHyperDocumentsArgs.CountAsync<HyperTag>(this._store, countArgs);
 
-            var conditions = await MetaDataSetHelper.GenerateFilterFromMetaDataSetAsync(_store, _metadataSetId);
-            findArgs.DescriptorConditions.AddCondition(conditions);
-            findArgs.Skip = PageSize * (PageNumber - 1);
-            findArgs.Limit = PageSize;
+         TotalPages.Value = totalTags % PageSize == 0 ? totalTags / PageSize : totalTags / (PageSize + 1);
+      }
 
-            var docs = await _store.ExecuteAsync(findArgs);
+      public async Task LoadHyperTags()
+      {
+         var findArgs = new FindHyperDocumentsArgs(typeof(HyperTag));
 
-            var hyperTags = new List<HyperTag>();
-            foreach (var doc in docs)
-            {
-                hyperTags.Add(doc.GetPayload<HyperTag>());
-            }
+         var conditions = await MetaDataSetHelper.GenerateFilterFromMetaDataSetAsync(_store, _metadataSetId);
+         findArgs.DescriptorConditions.AddCondition(conditions);
+         findArgs.Skip = PageSize * (PageNumber - 1);
+         findArgs.Limit = PageSize;
 
-            HyperTags.Value = hyperTags;
-        }
+         var docs = await _store.ExecuteAsync(findArgs);
 
-        public async Task ChangePage(int pageNumber)
-        {
-            PageNumber.Value = pageNumber;
+         var hyperTags = new List<HyperTag>();
+         foreach (var doc in docs)
+         {
+            hyperTags.Add(doc.GetPayload<HyperTag>());
+         }
 
-            HyperTags.Value = new List<HyperTag>();
+         HyperTags.Value = hyperTags;
+      }
 
-            await LoadHyperTags();
-        }
-    }
+      public async Task ChangePage(int pageNumber)
+      {
+         if (CurrentLoadedPage == pageNumber)
+            return;
+
+         CurrentLoadedPage = pageNumber;
+
+         PageNumber.Value = pageNumber;
+
+         HyperTags.Value = new List<HyperTag>();
+
+         await LoadHyperTags();
+      }
+   }
 }
