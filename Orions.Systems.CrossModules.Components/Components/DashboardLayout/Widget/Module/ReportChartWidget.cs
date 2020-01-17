903813399a73f5a8b743f339b948a3363a7f9c79
deyan <deyan@orionswave.com>
2020-01-17 19:52:47 +0200

    Tweaks

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/ReportChartWidget.cs
@@ -0,0 +1,20 @@
+﻿using Orions.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public abstract class ReportChartWidget : ReportBaseWidget
+	{
+		[HelpText("When you click on a chart, should the click apply a DAteTime filter based on what we clicked to the selected Dashboard filter group")]
+		public bool InteractiveModeDateTimeFiltering { get; set; } = true;
+
+		[HelpText("When you click on a chart, should the click apply a text-category filter based on what we clicked to the selected Dashboard filter group")]
+		public bool InteractiveModeCategoryFiltering { get; set; } = true;
+
+		public ReportChartWidget()
+		{
+		}
+	}
+}
