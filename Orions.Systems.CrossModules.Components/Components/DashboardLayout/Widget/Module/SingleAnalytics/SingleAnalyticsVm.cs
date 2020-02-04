498455c0ab1fbeca03381957d63867c6b4ca22fa
Venelin Kehayov <venelin@orionswave.com>
2020-02-04 15:59:02 +0100

    Added static single analytics widget

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SingleAnalytics/SingleAnalyticsVm.cs
@@ -0,0 +1,15 @@
+﻿using Orions.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	[Config(typeof(SingleAnalyticsWidget))]
+	public class SingleAnalyticsVm : WidgetVm<SingleAnalyticsWidget>
+	{
+		public SingleAnalyticsVm()
+		{
+		}
+	}
+}
---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SingleAnalytics/SingleAnalyticsWidget.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Orions.Systems.CrossModules.Components
+{
+   public class SingleAnalyticsWidget : DashboardWidget, IDashboardWidget
+   {
+      public class SingleAnalyticsItem 
+      {
+         public string Title { get; set; }
+
+         public string Value { get; set; }
+
+         public string IconHtml { get; set; }
+      }
+
+      public List<SingleAnalyticsItem> Items { get; set; } = new List<SingleAnalyticsItem>();
+
+      public SingleAnalyticsWidget()
+      {
+         this.Label = "Single Analytics Widget";
+      }
+   }
+}
