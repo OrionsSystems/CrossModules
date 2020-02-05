538c34fc1bc58c5fafba46fdeb5375505c76206a
Venelin Kehayov <venelin@orionswave.com>
2020-02-05 20:12:44 +0100

    Update single value card widget to load data from report

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SingleAnalytics/SingleAnalyticsItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Orions.Systems.CrossModules.Components
+{
+	public class SingleAnalyticsItem
+	{
+		public string Title { get; set; }
+
+		public string Value { get; set; }
+
+		public string IconHtml { get; set; }
+	}
+}
