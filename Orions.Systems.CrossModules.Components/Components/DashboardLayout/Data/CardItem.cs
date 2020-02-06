fc1c308107637a0d8d9772e3abf65223661fb5f5
Venelin Kehayov <venelin@orionswave.com>
2020-02-06 18:13:15 +0100

    Rename SignleAnalyticsItem to CardItem

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/CardItem.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public class CardItem
+	{
+		public string Title { get; set; }
+
+		public string Value { get; set; }
+
+		public int? IntValue
+		{
+			get
+			{
+				var value = DoubleValue;
+				if (value == null) return null;
+
+				return Convert.ToInt32(value);
+			}
+		}
+
+		public double? DoubleValue
+		{
+			get
+			{
+				double value;
+				var result = double.TryParse(Value, out value);
+				if (!result) return null;
+				return value;
+			}
+		}
+
+		public int Percentage { get; set; }
+
+		public string IconHtml { get; set; }
+	}
+}
--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SingleAnalytics/SingleAnalyticsWidget.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SingleAnalytics/SingleAnalyticsWidget.cs
@@ -4,7 +4,7 @@ namespace Orions.Systems.CrossModules.Components
 {
    public class SingleAnalyticsWidget : ReportChartWidget, IDashboardWidget
    {
-      public List<SingleAnalyticsItem> CustomItems { get; set; } = new List<SingleAnalyticsItem>();
+      public List<CardItem> CustomItems { get; set; } = new List<CardItem>();
 
       public bool IsShowCardIcons { get; set; }
 
