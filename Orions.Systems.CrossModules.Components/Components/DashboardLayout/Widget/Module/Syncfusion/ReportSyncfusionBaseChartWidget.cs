6ed5b449f126bf59526ff26c20e45275991a6a5f
Venelin Kehayov <venelin@orionswave.com>
2020-02-07 22:32:35 +0100

    Add cynfusion series widget properties

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Syncfusion/ReportSyncfusionBaseChartWidget.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Syncfusion/ReportSyncfusionBaseChartWidget.cs
@@ -5,22 +5,58 @@ namespace Orions.Systems.CrossModules.Components
 {
 	public abstract class ReportSyncfusionBaseChartWidget : ReportChartWidget, IDashboardWidget
 	{
+		public class ChartMargin 
+		{
+			public int Top { get; set; }
+			public int Left { get; set; }
+			public int Right { get; set; }
+			public int Bottom { get; set; }
+		}
+
+		public class PrimaryYAxis {
+			public int Minimum { get; set; }
+
+			//public int Interval { get; set; } = 1;
+			//public int Maximum { get; set; }
+
+			public ChartAxisLineStyle LineStyle { get; set; } = new ChartAxisLineStyle();
+
+			public ChartAxisMajorTickLines TickLines { get; set; } = new ChartAxisMajorTickLines();
+
+			public ChartAxisMajorGridLines GridLineSettings { get; set; } = new ChartAxisMajorGridLines();
+		}
+
+		public class PrimaryXAxis
+		{
+			public bool XAxisIsIndexed { get; set; } = true;
+
+			public LabelPlacement LabelPlacement { get; set; } = LabelPlacement.OnTicks;
+
+			public IntervalType IntervalType { get; set; } = IntervalType.Auto;
+
+			public string LabelFormat { get; set; } = "dd MMM HH:mm";
+
+			public string Skeleton { get; set; } = "Ed";
+
+			public EdgeLabelPlacement EdgeLabelPlacement { get; set; } = EdgeLabelPlacement.Shift;
+
+			public ChartAxisMajorGridLines GridLineSettings { get; set; } = new ChartAxisMajorGridLines();
+
+			//public int Minimum { get; set; }
+			//public int Maximum { get; set; }
+
+			public int Interval { get; set; } = 1;
+
+			public AxisPosition TickPosition { get; set; } = AxisPosition.Inside;
+
+			public AxisPosition LabelPosition { get; set; } = AxisPosition.Outside;
+		}
+
 		public bool IsShowChartTitle { get; set; } = false;
 
 		public string ChartTitle { get; set; }
 
-		public string LabelFormat { get; set; } = "dd MMM HH:mm";
-
 		public SelectionMode SelectionMode { get; set; } = SelectionMode.Series;
-		public string Skeleton { get; set; } = "Ed";
-
-		public EdgeLabelPlacement EdgeLabelPlacement { get; set; } = EdgeLabelPlacement.Shift;
-
-		public IntervalType IntervalType { get; set; } = IntervalType.Auto;
-
-		public LabelPlacement LabelPlacement { get; set; } = LabelPlacement.OnTicks;
-
-		public bool XAxisIsIndexed { get; set; } = true;
 
 		public bool IsEnableTooltip { get; set; } = true;
 
@@ -43,6 +79,16 @@ namespace Orions.Systems.CrossModules.Components
 		public bool EnableScrollbarZooming { get; set; }
 
 		public bool IsEnableCrosshair { get; set; }
+
+		public PrimaryXAxis XAxisSettings { get; set; } = new PrimaryXAxis();
+
+		public PrimaryYAxis YAxisSettings { get; set; } = new PrimaryYAxis();
+
+		public ChartCornerRadius CornerRadius { get; set; } = new ChartCornerRadius();
+
+		public ChartMargin Margin { get; set; } = new ChartMargin();
+
+		
 
 		public ReportSyncfusionBaseChartWidget()
 		{
