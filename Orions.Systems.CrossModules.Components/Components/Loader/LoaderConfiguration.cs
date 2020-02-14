9e033ff443a1a869bdb93f11e89ace6b56a2b2e2
Venelin Kehayov <venelin@orionswave.com>
2020-02-14 14:38:03 +0100

    Add loader component

---
+++ Orions.Systems.CrossModules.Components/Components/Loader/LoaderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Orions.Common;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public class LoaderConfiguration
+	{
+		[HelpText("Show or hide the loader")]
+		public bool Visible { get; set; } = true;
+
+		[HelpText("Separator height in px")]
+		public int Height { get; set; } = 2;
+
+		[HelpText("Valid CSS color string")]
+		public string Color { get; set; } = "#365c7e";
+
+		[HelpText("The opacity of the marker.")]
+		public double Opacity { get; set; } = 1;
+
+		[HelpText("Add styles to title")]
+		public string InlineStyles { get; set; }
+	}
+}
