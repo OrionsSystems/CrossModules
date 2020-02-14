25424f39b70b5204d401b87d329c456cfebc3f8d
Venelin Kehayov <venelin@orionswave.com>
2020-02-14 14:41:10 +0100

    Add settings to simple filter

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Filter/SimpleFilterConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Orions.Common;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public class SimpleFilterConfiguration
+	{
+		public enum SimpleFilterOrientation
+		{ 
+			Inline,
+			Block
+		}
+
+		[HelpText("Filter view orientation")]
+		public SimpleFilterOrientation Orientation { get; set; } = SimpleFilterOrientation.Inline;
+
+		[HelpText("Show or hide the loader")]
+		public bool Visible { get; set; } = true;
+
+		[HelpText("Separator height in px")]
+		public int Height { get; set; } = 2;
+
+		[HelpText("Valid CSS color string")]
+		public string Color { get; set; } = "#365c7e";
+
+	}
+}
