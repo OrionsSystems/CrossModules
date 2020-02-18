3ab9575303627916ece526a98932295a6686cf3b
Venelin Kehayov <velialarm@gmail.com>
2020-02-18 17:56:49 +0100

    Add datasource prefix fiter

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/DataSource/WidgetDataSource.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/DataSource/WidgetDataSource.cs
@@ -1,9 +1,7 @@
 ﻿using Orions.Common;
 using Orions.Infrastructure.Reporting;
-using Orions.Node.Common;
-using System;
-using System.Collections.Generic;
-using System.Text;
+
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Orions.Systems.CrossModules.Components
@@ -16,6 +14,15 @@ namespace Orions.Systems.CrossModules.Components
 
 		public IconMapping IconMapping { get; set; } = new IconMapping();
 
+		[HelpText("Include categories by enter comma names")]
+		public string IncludeCategories { get; set; }
+
+		[HelpText("Exclude categories by enter comma names")]
+		public string ExcludeCategories { get; set; }
+
+		[HelpText("Remove prefix from all categories if find it")]
+		public string ExcludePrefix { get; set; }
+
 		public WidgetDataSource()
 		{
 		}
@@ -26,6 +33,15 @@ namespace Orions.Systems.CrossModules.Components
 
 			if (this.Mapping != null)
 				result?.MapNames(this.Mapping);
+
+
+			if (!string.IsNullOrWhiteSpace(this.ExcludePrefix))
+			{
+				foreach (var col in result.ColumnsDefinitions ?? Enumerable.Empty<ReportColumnTemplate>())
+				{
+					col.Title = col.Title.Replace(this.ExcludePrefix, "");
+				}
+			}
 
 			return new Report[] { result };
 		}
