22a4cb5607f90a8c6767b3c8816ccb8f04d952e9
Venelin Kehayov <velialarm@gmail.com>
2020-02-25 01:51:28 +0100

    Update row command buttons position and add slide widget container

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SlideContainer/SlideContainerVm.cs
@@ -0,0 +1,12 @@
+﻿using Orions.Common;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	[Config(typeof(SlideContainerWidget))]
+	public class SlideContainerVm : WidgetVm<SlideContainerWidget>
+	{
+		public SlideContainerVm()
+		{
+		}
+	}
+}
---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/SlideContainer/SlideContainerWidget.cs
@@ -0,0 +1,12 @@
+﻿namespace Orions.Systems.CrossModules.Components
+{
+   public class SlideContainerWidget : DashboardWidget
+   {
+      public DashboardWidget[] Data { get; set; } = new DashboardWidget[] { };
+
+      public SlideContainerWidget()
+      {
+         this.Label = "Slide Container";
+      }
+   }
+}
