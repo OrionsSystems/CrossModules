ea30508383e49107c3c97eaa7f386a4876bc1b34
Venelin Kehayov <velialarm@gmail.com>
2020-02-28 13:19:02 +0100

    Add import and export dashboard

--- Orions.Systems.CrossModules.Components/content/scripts/common.js
+++ Orions.Systems.CrossModules.Components/content/scripts/common.js
@@ -441,3 +441,16 @@ window.addEventListener("resize", function () {
       }
    };
 })();
+
+(function () {
+   window.Orions.FileSaveAs = {
+      init: function (filename, fileContent) {
+         var link = document.createElement('a');
+         link.download = filename;
+         link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(fileContent);
+         document.body.appendChild(link);
+         link.click();
+         document.body.removeChild(link);
+      }
+   };
+})();
