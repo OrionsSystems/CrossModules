c620a58f63d46e8a7e818151081b8ea498a7755e
deyan <deyan@orionswave.com>
2020-03-04 22:51:45 +0200

    Carousel + Blazor Strap

---
+++ Orions.Systems.CrossModules.Components/Components/Carousel/CarouselVm.cs
@@ -0,0 +1,16 @@
+﻿
+using Orions.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	[Config(typeof(CarouselWidget))]
+	public class CarouselVm : WidgetVm<CarouselWidget>
+	{
+		public CarouselVm()
+		{
+		}
+	}
+}
---
+++ Orions.Systems.CrossModules.Components/Components/Carousel/CarouselWidget.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public class CarouselWidget : DashboardWidget
+	{
+		public CarouselWidget()
+		{
+			this.Label = "Carousel Widget";
+		}
+	}
+}
