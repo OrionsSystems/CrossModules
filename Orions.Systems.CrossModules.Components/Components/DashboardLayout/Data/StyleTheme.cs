c759da42e54cab84bc1de98c4d3727aa8359eecc
Venelin Kehayov <velialarm@gmail.com>
2020-03-07 00:55:41 +0100

    Introduce css themes

---
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/StyleTheme.cs
@@ -0,0 +1,31 @@
+﻿using Orions.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Orions.Systems.CrossModules.Components
+{
+   public class StyleTheme : IdUnifiedBlob, IName, IGroup
+   {
+      [DocumentDescriptor]
+      public string Name { get; set; } = "New Theme";
+
+      [DocumentDescriptor]
+      public string Group { get; set; }
+
+      [DocumentDescriptor]
+      public string Tag { get; set; }
+
+      [HelpText("Apply css styles to the bottom of the page")]
+      [UniBrowsable(UniBrowsableAttribute.EditTypes.MultiLineText)]
+      public string Styles { get; set; }
+
+      public StyleTheme()
+      {
+      }
+
+      public override string ToString()
+      {
+         return this.Name;
+      }
+   }
+}
