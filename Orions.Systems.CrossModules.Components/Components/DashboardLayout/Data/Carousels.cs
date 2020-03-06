986803091e947df3112fc1e58b90a1fe920956ac
pzadafiya <pzadafiya@gmail.com>
2020-03-06 18:49:49 +0530

    changes in Carousel
    Signed-off-by: Pranav <Pranav@orionswave.com>

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/Carousels.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/Carousels.cs
@@ -12,31 +12,4 @@ namespace Orions.Systems.CrossModules.Components
         public string ActionLink { get; set; }
         public string ActionLinkTarget { get; set; }
     }
-
-    public class ReportData
-    {
-        public int RowCount { get; set; }
-        public List<ReportSeriesData> Series { get; private set; } = new List<ReportSeriesData>();
-
-        public void Clean()
-        {
-            Series.Clear();
-        }
-    }
-
-    public class ReportSeriesData
-    {
-        public string HeaderTitle { get; set; }
-        public List<ReportSeriesDataItem> Data { get; set; } = new List<ReportSeriesDataItem>();
-    }
-
-    public class ReportSeriesDataItem
-    {
-        public string Name { get; set; }
-        public string Value { get; set; }
-        public ReportSeriesDataItem()
-        {
-        }
-
-    }
 }
