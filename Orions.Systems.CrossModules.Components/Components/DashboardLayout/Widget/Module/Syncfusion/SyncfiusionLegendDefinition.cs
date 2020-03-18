9c3a26005d29f74fdf9836a588e5b1a1eb3ed493
Venelin Kehayov <velialarm@gmail.com>
2020-03-18 14:49:48 +0100

    Update acumulation legend settings

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Syncfusion/SyncfiusionLegendDefinition.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Widget/Module/Syncfusion/SyncfiusionLegendDefinition.cs
@@ -1,12 +1,45 @@
-﻿using Syncfusion.EJ2.Blazor.Charts;
+﻿using Orions.Common;
+using Syncfusion.EJ2.Blazor.Charts;
 
 namespace Orions.Systems.CrossModules.Components
 {
 	public class SyncfiusionLegendDefinition
 	{
+		[HelpText("If set to true, legend will be visible.")]
 		public bool Visible { get; set; } = true;
+		
+		[HelpText("Position of the legend in the chart")]
 		public LegendPosition Position { get; set; } = LegendPosition.Top;
+
+		[HelpText("Legend in chart can be aligned ")]
 		public Alignment Alignment { get; set; } = Alignment.Far;
+		
+		[HelpText("Options to customize the legend text.")]
+		public AccumulationChartLegendFont TextStyle { get; set; }
+
+		[HelpText("Shape width of the legend in pixels.")]
+		public double ShapeWidth { get; set; } = 10;
+
+		[HelpText("Padding between the legend shape and text.")]
+		public double ShapePadding { get; set; } = 5;
+
+		[HelpText("Shape height of the legend in pixels.")]
+		public double ShapeHeight { get; set; } = 10;
+
+		[HelpText("Option to customize the padding between legend items.")]
+		public double Padding { get; set; } = 8;
+
+		[HelpText("Opacity of the legend.")]
+		public double Opacity { get; set; } = 1;
+
+		[HelpText("Options to customize left, right, top and bottom margins of the chart.")]
+		public MarginModel Margin { get; set; }
+
+		[HelpText("The height of the legend in pixels.")]
+		public string Height { get; set; }
+
+		[HelpText(" The width of the legend in pixels.")]
+		public string Width { get; set; }
 
 	}
 }
