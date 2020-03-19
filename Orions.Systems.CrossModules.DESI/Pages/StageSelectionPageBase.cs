10541d3d0dd75b161a9dd7deb4c87b8e10109e10
Andrei Sasimovich <ballack.tyta@gmail.com>
2020-03-19 05:00:59 +0300

    Desi: added stage selection screen

--- Orions.Systems.CrossModules.DESI/Pages/StageSelectionPageBase.cs
+++ Orions.Systems.CrossModules.DESI/Pages/StageSelectionPageBase.cs
@@ -3,20 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Orions.Systems.CrossModules.Desi.Infrastructure;
+using Orions.Systems.Desi.Common.Authentication;
+using Orions.Systems.Desi.Common.Models;
 using Orions.Systems.Desi.Core.ViewModels;
 
 namespace Orions.Systems.CrossModules.Desi.Pages
 {
 	public class StageSelectionPageBase : DesiBaseComponent<StageSelectionViewModel>
 	{
-		protected override Task OnInitializedAsync()
+		protected override async Task OnInitializedAsync()
 		{
-			var vm = new StageSelectionViewModel(
+			var authSystem = DependencyResolver.GetAuthenticationSystem();
+			var navigationService = DependencyResolver.GetNavigationService();
+
+			if (authSystem.Store.Data.AuthenticationStatus == AuthenticationStatus.LoggedOut)
+			{
+				await navigationService.GoToLoginPage();
+				await base.OnInitializedAsync();
+
+				return;
+			}
+
+			this.Vm = new StageSelectionViewModel(
 				DependencyResolver.GetMissionsExploitationSystem(),
 				DependencyResolver.GetNavigationService()
 			);
 
-			return base.OnInitializedAsync();
+			await base.OnInitializedAsync();
+		}
+
+		public void Switch_Toggled(PhaseModel phase)
+		{
+			phase.IsSelected = !phase.IsSelected;
+			this.Vm.ValidateStageSelection();
 		}
 	}
 }
