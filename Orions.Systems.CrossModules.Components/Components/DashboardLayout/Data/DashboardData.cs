e53fec7c74acd7dc7d103390a977a7b8c3fa91ac
Venelin Kehayov <velialarm@gmail.com>
2020-03-20 16:52:47 +0100

    Add dashboard list grouping mode

--- Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/DashboardData.cs
+++ Orions.Systems.CrossModules.Components/Components/DashboardLayout/Data/DashboardData.cs
@@ -5,73 +5,85 @@ using System.Collections.Generic;
 
 namespace Orions.Systems.CrossModules.Components
 {
-   public class DashboardData : IdUnifiedBlob, IName, IGroup
-   {
-      [DocumentDescriptor]
-      public string Name { get; set; } = "New Dashboard";
+	public class DashboardData : IdUnifiedBlob, IName, IGroup
+	{
+		[DocumentDescriptor]
+		public string Name { get; set; } = "New Dashboard";
 
-      [DocumentDescriptor]
-      public string Group { get; set; }
+		[DocumentDescriptor]
+		public string Group { get; set; }
 
-      [HelpText("Enable client access to dashboard")]
-      [DocumentDescriptor]
-      public bool Published { get; set; }
+		[HelpText("Enable client access to dashboard")]
+		[DocumentDescriptor]
+		public bool Published { get; set; }
 
-      [DocumentDescriptor]
-      public bool IsHideTitle { get; set; } = true;
+		[DocumentDescriptor]
+		public bool IsHideTitle { get; set; } = true;
 
-      [DocumentDescriptor]
-      public string Tag { get; set; }
+		[DocumentDescriptor]
+		public string Tag { get; set; }
 
-      [HelpText("Apply css styles to the bottom of the page")]
-      [UniBrowsable(UniBrowsableAttribute.EditTypes.MultiLineText)]
-      public string Styles { get; set; }
+		[HelpText("Apply css styles to the bottom of the page")]
+		[UniBrowsable(UniBrowsableAttribute.EditTypes.MultiLineText)]
+		public string Styles { get; set; }
 
 
-      [HelpText("Attach theme to dashboard", HelpTextAttribute.Priorities.Optional)]
-      [HyperDocumentId.DocumentType(typeof(StyleTheme))]
-      public HyperDocumentId? Theme { get; set; }
+		[HelpText("Attach theme to dashboard", HelpTextAttribute.Priorities.Optional)]
+		[HyperDocumentId.DocumentType(typeof(StyleTheme))]
+		public HyperDocumentId? Theme { get; set; }
 
-      [DocumentDescriptor]
-      public bool EnableStyles { get; set; } = true;
+		[DocumentDescriptor]
+		public bool EnableStyles { get; set; } = true;
 
-      public LinkedList<DashboardRow> Rows { get; set; } = new LinkedList<DashboardRow>();
+		public LinkedList<DashboardRow> Rows { get; set; } = new LinkedList<DashboardRow>();
 
-      public DashboardData()
-      {
-      }
+		[UniBrowsable(false)]
+		public string DefaultGroup
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(Group)) 
+					return "Default";
 
-      public override string ToString()
-      {
-         return this.Name;
-      }
-   }
+				return Group;
+			}
+		}
 
-   public class DashboardRow : IId
-   {
-      public string Id { get; set; } = IdGeneratorHelper.Generate("db-row-");
+		public DashboardData()
+		{
+		}
 
-      public LinkedList<DashboardColumn> Columns { get; set; }
+		public override string ToString()
+		{
+			return this.Name;
+		}
+	}
 
-      public bool ShowCommands { get; set; }
+	public class DashboardRow : IId
+	{
+		public string Id { get; set; } = IdGeneratorHelper.Generate("db-row-");
 
-      public DashboardRow()
-      {
-         Columns = new LinkedList<DashboardColumn>();
-      }
-   }
+		public LinkedList<DashboardColumn> Columns { get; set; }
 
-   public class DashboardColumn : IId
-   {
-      public string Id { get; set; } = IdGeneratorHelper.Generate("db-col-");
-      public int Size { get; set; }
-      public int Order { get; set; }
-      public bool ShowCommands { get; set; }
+		public bool ShowCommands { get; set; }
 
-      public LinkedList<DashboardRow> InnerRows { get; set; } = new LinkedList<DashboardRow>();
+		public DashboardRow()
+		{
+			Columns = new LinkedList<DashboardColumn>();
+		}
+	}
 
-      public bool ShowBetweenCommands { get; set; }
+	public class DashboardColumn : IId
+	{
+		public string Id { get; set; } = IdGeneratorHelper.Generate("db-col-");
+		public int Size { get; set; }
+		public int Order { get; set; }
+		public bool ShowCommands { get; set; }
 
-      public DashboardWidget Widget { get; set; }
-   }
+		public LinkedList<DashboardRow> InnerRows { get; set; } = new LinkedList<DashboardRow>();
+
+		public bool ShowBetweenCommands { get; set; }
+
+		public DashboardWidget Widget { get; set; }
+	}
 }
