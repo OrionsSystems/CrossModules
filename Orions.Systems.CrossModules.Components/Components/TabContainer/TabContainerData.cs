e2e053f32ae14d89cbc01a379417e2a0581ddc61
Venelin Kehayov <velialarm@gmail.com>
2020-03-24 21:26:22 +0100

    Into tab container component

---
+++ Orions.Systems.CrossModules.Components/Components/TabContainer/TabContainerData.cs
@@ -0,0 +1,15 @@
+﻿using Orions.Common;
+
+namespace Orions.Systems.CrossModules.Components
+{
+	public class TabContainerData
+	{
+		public string Name { get; set; }
+
+		public bool Active { get; set; }
+
+		public bool Enable { get; set; }
+
+		public bool Action { get; set; }
+	}
+}
