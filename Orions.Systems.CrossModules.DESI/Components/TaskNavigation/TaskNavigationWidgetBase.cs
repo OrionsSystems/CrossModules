df2afa1cd9c53d21f7e389f8381ceaa5f3141230
Anton Emelyancev <anton@orionswave.com>
2020-03-24 14:18:27 +0300

    Copy VizList and TaskNavigation components and styles to Desi.CrossModules.

---
+++ Orions.Systems.CrossModules.DESI/Components/TaskNavigation/TaskNavigationWidgetBase.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+using Orions.Systems.Desi.Common.General;
+using Orions.Systems.Desi.Common.Models;
+using Orions.Systems.Desi.Common.TaskExploitation;
+using Orions.Systems.Desi.Common.Util;
+
+namespace Orions.Systems.CrossModules.Desi.Components.TaskNavigation
+{
+	public class TaskNavigationWidgetBase: ComponentBase, IDisposable
+	{
+		private IDisposable _dataTracker;
+		private TaskExploitationData _data;
+
+		[Parameter]
+		public TaskExploitationData Data
+		{
+			get => _data;
+			set
+			{
+				_data = value;
+				_dataTracker?.Dispose();
+				_dataTracker = new PropertyTracker(Data, () => InvokeAsync(StateHasChanged));
+			}
+		}
+
+		[Parameter]
+		public ITaskExploitationController Controller { get; set; }
+		[Parameter]
+		public IActionDispatcher ActionDispatcher { get; set; }
+
+		protected void GoToNextTask() => Controller.Dispatch(GoToNextTaskAction.Create());
+		protected void GoToPrevTask() => Controller.Dispatch(GoToPreviousTaskAction.Create());
+		protected void SetCurrentTask(TaskModel task) => Controller.Dispatch(SetCurrentTaskAction.Create(task));
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+					_dataTracker?.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~TaskNavigationWidgetBase()
+		// {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
---
+++ Orions.Systems.CrossModules.DESI/Components/VizList/VizListComponentBase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Reactive.Linq;
+using Microsoft.AspNetCore.Components;
+using Orions.Infrastructure.HyperSemantic;
+using Orions.Systems.Desi.Common.Extensions;
+using Orions.Systems.Desi.Common.General;
+using Orions.Systems.Desi.Common.TagonomyExecution;
+
+namespace Orions.Systems.CrossModules.Desi.Components.VizList
+{
+	public class VizListComponentBase : ComponentBase, IDisposable
+	{
+		private TagonomyExecutionData _data;
+		private IDisposable _userActionStartedSub;
+
+		[Parameter]
+		public TagonomyExecutionData Data
+		{
+			get => _data;
+			set
+			{
+				_data = value;
+				_userActionStartedSub?.Dispose();
+				_userActionStartedSub = value?
+					.GetPropertyChangedObservable()
+					.Where(i => i.EventArgs.PropertyName == nameof(TagonomyExecutionData.PendingUserAction)
+					&& i.Source.PendingUserAction != null)
+					.Subscribe(_ => InvokeAsync(StateHasChanged));
+
+				StateHasChanged();
+			}
+		}
+		[Parameter]
+		public IActionDispatcher ActionDispatcher { get; set; }
+		[Parameter]
+		public ITagonomyExecutionController Controller { get; set; }
+
+		protected void SelectTagonomyNode(TagonomyNodeModel tagonomyNodeModel) => Controller.Dispatch(SelectTagonomyNodeAction.Create(tagonomyNodeModel));
+		protected void FinishTagonomyExecution() => Controller.Dispatch(FinishTagonomyExecutionAction.Create());
+		protected void StepBack() => Controller.Dispatch(TagonomyPathAction.Create(TagonomyPathAction.Actions.StepBack));
+		protected void GoToStep(TagonomyExecutionStep tagonomyExecutionStep) => Controller.Dispatch(GoToStepTagonomyAction.Create(tagonomyExecutionStep));
+		protected void FinishCurrentPath() => Controller.Dispatch(TagonomyPathAction.Create(TagonomyPathAction.Actions.FinishCurrentPath));
+		protected void SkipCurrentPath() => Controller.Dispatch(TagonomyPathAction.Create(TagonomyPathAction.Actions.SkipCurrentPath));
+		protected void SelectTagonomyProperty(TagonomyPropertyModel tagonomyPropertyModel) => Controller.Dispatch(SelectTagonomyPropertyAction.Create(tagonomyPropertyModel));
+		protected void FinishInputAction(string value) => Controller.Dispatch(FinishTagonomyInputAction.Create(value));
+		protected void FinishInputAction(bool value) => Controller.Dispatch(FinishTagonomyConfirmationAction.Create(value));
+
+		#region IDisposable Support
+		private bool disposedValue = false; // To detect redundant calls
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue)
+			{
+				if (disposing)
+				{
+					// TODO: dispose managed state (managed objects).
+					_userActionStartedSub?.Dispose();
+				}
+
+				// TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+				// TODO: set large fields to null.
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+		// ~VizListComponentBase()
+		// {
+		//   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+		//   Dispose(false);
+		// }
+
+		// This code added to correctly implement the disposable pattern.
+		public void Dispose()
+		{
+			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+			Dispose(true);
+			// TODO: uncomment the following line if the finalizer is overridden above.
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
+	}
+}
