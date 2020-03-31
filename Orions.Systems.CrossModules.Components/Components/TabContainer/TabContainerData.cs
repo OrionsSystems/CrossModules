862a965401b955ab2609a38d2f913c7f9634de53
Venelin Kehayov <venelin@orionswave.com>
2020-03-31 19:04:28 +0200

    Update tab component content transitions

--- Orions.Systems.CrossModules.Components/Components/TabContainer/TabContainerData.cs
+++ Orions.Systems.CrossModules.Components/Components/TabContainer/TabContainerData.cs
@@ -10,6 +10,14 @@ namespace Orions.Systems.CrossModules.Components
 
 		public bool Enable { get; set; }
 
+		public string Class { get; set; }
+
+		public string AnimationClass { get; set; } 
+
+		public string OutTransitions { get; set; }
+
+		public string InTransitions { get; set; }
+
 		public bool Action { get; set; }
 	}
 }
