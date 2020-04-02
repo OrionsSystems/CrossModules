c7d0a57c885e1d94d31ab3006160d20da54109f5
Andrei Sasimovich <andrei@orionswave.com>
2020-04-02 15:19:26 +0300

    Desi: added client side validation to login page

---
+++ Orions.Systems.CrossModules.DESI/clientApp/src/login/index.js
@@ -0,0 +1,16 @@
+﻿window.Orions.LoginPage = {
+    initialize: function (componentRef) {
+		let logingFormEl = document.querySelector('.desi-login.form');
+        logingFormEl.addEventListener('submit', function (event) {
+            if (logingFormEl.checkValidity() === false) {
+                
+            }
+            else {
+                componentRef.invokeMethodAsync("Login");
+            }
+
+            logingFormEl.classList.add('was-validated');
+            event.preventDefault();
+        }, false);
+	}
+}
