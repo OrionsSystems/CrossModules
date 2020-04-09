468398451d65de0bf8c1946831b1e232b0ae3d07
Anton Emelyancev <anton@orionswave.com>
2020-04-09 20:13:19 +0300

    Implement dynamic header for MainLayout. Add refresh button for missions page.

---
+++ Orions.Systems.CrossModules.DESI/Infrastructure/Routes.cs
@@ -0,0 +1,10 @@
+﻿namespace Orions.Systems.CrossModules.Desi.Infrastructure
+{
+	public static class Routes
+	{
+		public const string Root = "";
+		public const string Missions = "missions";
+		public const string StageSelection = "stage-selection";
+		public const string Tagging = "tagging";
+	}
+}
