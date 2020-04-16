b23231f52b2c7dc52ed486dfe86eb9cd41229666
Venelin Kehayov <venelin@orionswave.com>
2020-04-16 19:47:49 +0200

    Update flow designer styles and remote data loader

--- Orions.Systems.CrossModules.Components/Components/FlowDesigner/FlowDesignerBase.cs
+++ Orions.Systems.CrossModules.Components/Components/FlowDesigner/FlowDesignerBase.cs
@@ -43,9 +43,67 @@ namespace Orions.Systems.CrossModules.Components
 
 		protected override async Task OnFirstAfterRenderAsync()
 		{
-			thisReference = DotNetObjectReference.Create(this);
-			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Init", new object[] { thisReference });
+			await Vm.Init();
+			var jsonData = Vm.GetJesonDesignData();
 
+			thisReference = DotNetObjectReference.Create(this);
+
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Init", new object[] { thisReference, jsonData });
+
+		}
+
+		public async Task ToggleCommonMenu() 
+		{
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.ToggleCommonMenu");
+		}
+
+		public async Task OnClickApply()
+		{ 
+			//TODO
+		}
+
+		public async Task ToggleMainMenu() 
+		{
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.ToggleMainMenu");
+		}
+
+		public async Task CopyElement()
+		{
+			thisReference = DotNetObjectReference.Create(this);
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Copy", new object[] { thisReference });
+		}
+
+		public async Task PasteElement()
+		{
+			thisReference = DotNetObjectReference.Create(this);
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Paste", new object[] { thisReference });
+		}
+
+		public async Task DuplicateElement()
+		{
+			thisReference = DotNetObjectReference.Create(this);
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Duplicate", new object[] { thisReference });
+		}
+
+		public async Task RemoveElement()
+		{
+			thisReference = DotNetObjectReference.Create(this);
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.Remove", new object[] { thisReference });
+		}
+
+		public async Task ZoomIn()
+		{
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.ZoomIn");
+		}
+
+		public async Task ZoomReset() 
+		{
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.ZoomReset");
+		}
+
+		public async Task ZoomOut() 
+		{
+			await JsInterop.InvokeAsync<object>("Orions.FlowDesigner.ZoomOut");
 		}
 
 		void IDisposable.Dispose()
--- Orions.Systems.CrossModules.Components/Components/FlowDesigner/FlowDesignerVm.cs
+++ Orions.Systems.CrossModules.Components/Components/FlowDesigner/FlowDesignerVm.cs
@@ -1,15 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Orions.Common;
 using Orions.Infrastructure.Common;
 using Orions.Infrastructure.HyperMedia;
 using Orions.Node.Common;
+using Orions.Systems.CrossModules.Components.Model;
 
 namespace Orions.Systems.CrossModules.Components
 {
 	public class FlowDesignerVm : BlazorVm
 	{
+		public HyperWorkflow Source { get; set; }
+
+		public WorkflowDesignData DesignData { get; private set; } 	 = new WorkflowDesignData();
+
+		/// <summary>
+		/// Statuses for this view model.
+		/// </summary>
+		public HyperWorkflowStatus[] WorkflowStatuses { get; set; }
+
+
 		public bool IsShowDesignerSetting { get; set; }
 
 		public IHyperArgsSink HyperStore { get; set; }
@@ -21,9 +34,113 @@ namespace Orions.Systems.CrossModules.Components
 			
 		}
 
-		public void LoadWorkflowDesignerData(string workflowId) 
+		public virtual async Task Init() 
+		{ 
+			var workflowId = "f4a66ad3-b538-4bcd-8e09-739e4b37e016";
+			var workflowInstanceId = "";
+
+			await PopulateWorkflow(workflowId);
+			await PopulateWorkflowStatuses(workflowInstanceId);
+
+			PopulateDesignerData();
+
+		}
+
+		public string GetJesonDesignData() {
+			return JsonConvert.SerializeObject(DesignData, WorkflowDesignConverter.Settings);
+		}
+
+		private void PopulateDesignerData() 
 		{
-			throw new NotImplementedException();
+			var design = new WorkflowDesignData();
+
+			var types = GetHyperWorkflowNodeDataType();
+
+			foreach (var nodeType in types)
+			{
+				var nodeName = nodeType.Name;
+				var nodeConfigId = String.Copy(nodeName);
+
+				nodeName = RemovePrefix(nodeName);
+
+				var nodeNameFull = nodeType.FullName;
+
+				var node = new NodeConfiguration(nodeType);
+				var inputCount = node.InputCount;
+				var outputCount = node.OutputCount;
+				var group = node.Group;
+				var color = UniColorToHex(node.Color);
+
+				var designNodeConfiguration = new DesignNodeConfiguration()
+				{
+					Id = nodeConfigId.ToLower(),
+					TypeFull = nodeNameFull,
+					Title = nodeName,
+					Input = inputCount,
+					Output = outputCount,
+				};
+
+				if (!string.IsNullOrEmpty(color)) designNodeConfiguration.Color = color;
+				if (!string.IsNullOrEmpty(group)) designNodeConfiguration.Group = group;
+
+				design.NodeConfigurations.Add(designNodeConfiguration);
+			}
+
+			// TODO : Save data in session !!!
+
+			design.WorkflowName = Source.Name;
+			design.WorkflowId = Source.Id;
+
+			foreach (NodeConfiguration nodeConfig in Source.Nodes)
+			{
+				//add node
+				var x = nodeConfig.GUIPosition.X;
+				var y = nodeConfig.GUIPosition.Y;
+
+				var color = UniColorToHex(nodeConfig.Color);
+				var group = nodeConfig.Group;
+
+				var component = new DesignComponent()
+				{
+					X = Convert.ToInt64(x),
+					Y = Convert.ToInt64(y),
+					Name = nodeConfig.NodeType.Name.ToLower(),
+					Id = nodeConfig.Id,
+					Type = nodeConfig.NodeType.TypeName,
+					State = new State()
+					{
+						Text = string.IsNullOrWhiteSpace(nodeConfig.Name) ? nodeConfig.NodeType.Name : nodeConfig.Name,
+						Color = color
+					},
+					Group = group
+				};
+
+				if (!string.IsNullOrEmpty(color)) component.State.Color = color;
+				if (!string.IsNullOrEmpty(group)) component.Group = group;
+
+				//connections
+				foreach (var connection in nodeConfig.OutputConnections.Where(it => it != null))
+				{
+					var source = Source.GetNodeConfiguration(connection.SourceId);
+					var target = Source.GetNodeConfiguration(connection.TargetId);
+
+					var sourceConnectionId = connection.SourceId;
+					var targetConnectionId = connection.TargetId;
+
+					var con = new Connection()
+					{
+						Id = targetConnectionId,
+						Index = "0"
+					};
+
+					component.Connections.Add(con);
+				}
+
+				design.Components.Add(component);
+			}
+
+			DesignData =  design;
+
 		}
 
 		public void LoadWorkflowStatuses(string workflowId, string workflowInstanceId)
@@ -37,20 +154,49 @@ namespace Orions.Systems.CrossModules.Components
 			throw new NotImplementedException();
 		}
 
-		public void SaveLocalNodeDesignData(string workflowId, string json)
+		private async Task PopulateWorkflow(string workflowId) 
 		{
-			throw new NotImplementedException();
+			if (HyperStore == null) return;
+
+			var documentId = HyperDocumentId.Create<HyperWorkflow>(workflowId);
+			var args = new RetrieveHyperDocumentArgs(documentId);
+			var doc = await HyperStore.ExecuteAsync(args);
+
+			if (args.ExecutionResult.IsNotSuccess)
+				return;
+
+			Source =  doc?.GetPayload<HyperWorkflow>();
 		}
 
-		public void CreateLocalNode(string workflowId, string desingComponentJson)
+		private async Task PopulateWorkflowStatuses(string workflowInstanceId) 
 		{
-			throw new NotImplementedException();
+
+			if (HyperStore == null) return;
+			if (string.IsNullOrWhiteSpace(workflowInstanceId)) return;
+
+			try 
+			{
+				var args = new RetrieveHyperWorkflowsStatusesArgs() { WorkflowInstancesIds = new string[] { workflowInstanceId } };
+
+				WorkflowStatuses = await HyperStore.ExecuteAsyncThrows(args);
+
+			} catch(Exception ex) {
+				Console.WriteLine(ex.Message);
+			}
+			// Pull all working statuses.
+			
 		}
 
-		public void DuplicateLocalNode(string workflowId, string originalNodeConfigId, string desingComponentJson)
+		private List<Type> GetHyperWorkflowNodeDataType()
 		{
-			throw new NotImplementedException();
+			List<Type> types = ReflectionHelper.Instance.GatherTypeChildrenTypesFromAssemblies(typeof(HyperWorkflowNodeData),
+				 ReflectionHelper.Instance.AllAssemblies);
+
+			types = types.Where(it => it.IsAbstract == false && it.GetCustomAttributes(true).Any(it2 => it2 is ObsoleteAttribute) == false).ToList();
+
+			return types;
 		}
+
 
 		private void SaveHyperWorkflowInSession(HyperWorkflow hyperWorkflow)
 		{
@@ -60,6 +206,30 @@ namespace Orions.Systems.CrossModules.Components
 		private HyperWorkflow LoadHyperWorkflowFromSession()
 		{
 			throw new NotImplementedException();
+		}
+
+		private string[] _remStr = new string[] { "HyperWorkflowNodeData", "WorkflowNodeData" };
+		private string RemovePrefix(string value) {
+			foreach (var sciptEndPrefix in _remStr)
+			{
+				if (value.EndsWith(sciptEndPrefix))
+				{
+					value = value.Substring(0, value.LastIndexOf(sciptEndPrefix));
+					return value;
+				}
+			}
+
+			return value;
+		}
+
+		private string UniColorToHex(UniColor color)
+		{
+			if (color.IsEmpty) return "#7d7d7d";
+
+			return string.Format("#{0:X2}{1:X2}{2:X2}",
+						color.R,
+						color.G,
+						color.B);
 		}
 
 	}
